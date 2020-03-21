5a4aac8d91cdf4ffe5b5b53be0032426335f3541
zongtong009 <zongtongguilai1@gmail.com>
2020-03-21 12:14:58 +0800

    1

---
+++ hello.java
@@ -0,0 +1,11 @@
+/**
+ * dasdfa
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println(165465.2165);
+        System.out.println("Hello, World\n"+"The weather's good today");
+    }
+}
+
